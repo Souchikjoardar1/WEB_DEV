541e3e269878b3d762355808f79289d0665bb671
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-08-13 13:48:08 +0530

    function to return sum of any no of args

--- Javascript/Param_pass.js
+++ Javascript/Param_pass.js
@@ -30,4 +30,17 @@ function a() {
     const array = Array.from(arguments)
     console.log("arguments converted into an array "+array);
 }
-a("souchik",22,"kolkata")//arguments is an array like data structure 
+a("souchik", 22, "kolkata")//arguments is an array like data structure 
+function b(...number) {
+    console.log(number);
+}
+b(1, 2, 3) //this will be convert any number of arguments passed into the function into an array 
+b("souchik", "joardar", "chikowski")
+// returning the sum of any number of arguments using Array.reduce()accumulator and reduce parameters
+function anysum(...numbers) {
+    let sum = numbers.reduce(function (acc, curr) {
+        return acc+curr
+    }, 0);
+    return sum
+}
+console.log("the sum of the numbers are: "+anysum(1, 2, 334, 45, 33));
