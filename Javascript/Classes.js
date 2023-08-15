d0a48f4c0809b6ffa1c3325155a596bcffa4e512
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-08-15 19:24:19 +0530

    learning about array functions and dates in JS

---
+++ Javascript/Classes.js
@@ -0,0 +1,13 @@
+class Railwayreversationform{
+    submit() {
+        alert('form submitted');
+}
+    cancel() {
+        alert('form cancelled');
+    }
+}
+let souchikform = new Railwayreversationform();//creating a form object by giving a template called class for a person
+let aritraform = new Railwayreversationform();
+souchikform.submit()
+aritra.submit()
+aritraform.cancel()
---
+++ Javascript/Dates.js
@@ -0,0 +1,6 @@
+const mydate = new Date();
+console.log(mydate);
+console.log(mydate.getDay());
+console.log(mydate.getTime());
+console.log(mydate.getMinutes());
+console.log(mydate.getHours());
