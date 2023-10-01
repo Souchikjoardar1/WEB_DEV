61af9f2e27d092fd646b5d82857d4641c59c5590
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-10-01 12:36:45 +0530

    learned some new concepts in functions JS

--- Javascript/Functions.js
+++ Javascript/Functions.js
@@ -1,22 +1,48 @@
-let age = 15
-console.log(`check if the person is above 18 label as adult`);
-if (age >= 18)
-    console.log("the person is an adult")
-else
-    console.log("the person is a minor")
-console.log("now let's check whether that person is a nigga!")
-const name = "souchik"
-let nigga = false;
-function isNigga() {
-    if (name == "chikowski")
-        nigga = true;
+let flag=false
+function addtwonumbers(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        flag = true;
+        return (`${num1}+${num2} = ` + (num1 + num2));
+    }
     else
-        console.log("the person is not a nigga!!")
+        console.log("Invalid input");
 }
-isNigga()
-let a = 10
-let b=5
-function multi(a, b) {
-    console.log("the product of "+a+" * "+b+" is= "+(a*b));
+let result = addtwonumbers(10, 'a')
+if (flag) {
+    console.log(result);
 }
-multi(a, b)
+else
+    result
+function loginusermessage(username) {
+    if (username)
+        return `welcome ${username}`;
+    else
+        return 'the user is not logged in';
+}
+console.log(loginusermessage());
+// !username because in JavaScript, undefined, null, 0, NaN, false, and an empty string ('') are all falsy values. Therefore, !username will return true if username is undefined, null, or an empty string.
+// how to pass multiple parametres in a function by using rest operator ...
+function calculatecartprice(...num1) {
+    return (num1);
+}
+console.log(calculatecartprice(23, 546, 345, 456));
+const user = {
+    username:'' ,
+    price:NaN
+}
+function handleobject(anyobject) {
+    if (anyobject.username && anyobject.price)
+        console.log(`the username is ${anyobject.username} and the price is ${anyobject.price}`);
+    else
+        console.log("enter proper values");
+}
+// handleobject(user)
+handleobject({ //alternative way of passing object keys and values in a function call 
+    username: 'souchik',
+    price: 1000
+})
+const mynewarray = [200, 300, 400]
+function returnsecondvalue(getnewArray) {
+    return getnewArray[1];    
+}
+console.log(returnsecondvalue(mynewarray));
