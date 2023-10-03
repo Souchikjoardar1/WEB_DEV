40bad18a430623ec72f75e207cb95975e1efc862
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2023-10-03 20:42:19 +0530

    learned about higher order array loops in JS to be continued

--- Javascript/Basics/tempCodeRunnerFile.js
+++ Javascript/Basics/tempCodeRunnerFile.js
@@ -1,4 +1,10 @@
-let index=0
-// while (index<10) {
-//     console.log("nigga!");
+let arr = ['superman', 'batman', 'spidernam'];
+// for(const i of arr) {
+//     console.log(i);
 // }
+// let m = new Map();
+// m.set(123,'souchik')
+// m.set(345, 'joardar')
+// // for (const i [key,value] of m) {
+    
+// // }
