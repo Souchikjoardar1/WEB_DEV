149a32e44921c35e57c085acd4d497990d7008df
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2024-04-21 11:09:18 +0530

    simple changes

---
+++ Javascript/Basics/Revision/Functions.js
@@ -0,0 +1,12 @@
+let a = 10
+let b = 40
+// non parameterized
+function add() {
+    return(a+b);
+}
+console.log(add())
+// parameterized
+function add1(a,b) {
+    return(a+b)
+}
+console.log(add1(234,345))
