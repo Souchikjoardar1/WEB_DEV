5b21d06c88f402cf33e60d1d9b97e0babaad3547
Souchik Joardar <souchik.joardar.fiem.cse20@teamfuture.in>
2024-05-14 12:29:16 +0530

    corrected the logical errors

--- Javascript/Basics/Revision/Functions.js
+++ Javascript/Basics/Revision/Functions.js
@@ -1,15 +1,23 @@
-let a = 10
-let b = 40
-// non parameterized
-function add() {
-    return(a+b);
+// let a = 10
+// let b = 40
+// // non parameterized
+// function add() {
+//     return(a+b);
+// }
+// console.log(add())
+// // parameterized
+// function add1(a,b) {
+//     return(a+b)
+// }
+// console.log(add1(234, 345))
+// let name = "souchik"
+// n=name.slice(2,5)
+// console.log(n);
+function isleapyear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        console.log("the year " + year + " is a leap year");
+    } else {
+        console.log("the year " + year + " is not a leap year");
+    }
 }
-console.log(add())
-// parameterized
-function add1(a,b) {
-    return(a+b)
-}
-console.log(add1(234, 345))
-let name = "souchik"
-n=name.slice(2,5)
-console.log(n);
+isleapyear(1968)
